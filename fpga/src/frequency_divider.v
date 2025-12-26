0751170fcc86a507cfc4d34881454ce7e0af8267
Fabien Soulier <fabien.soulier@umontpellier.fr>
2025-12-26 19:24:07 +0100

    Make reset of freq. divider asynchronous.

--- fpga/src/frequency_divider.v
+++ fpga/src/frequency_divider.v
@@ -1,21 +1,23 @@
 module frequency_divider(
     input wire clk, // Input clock  
     input wire rst, // Asynchronous reset
-    output reg  clk2i,  // Output clock (in-phase)
-    output reg  clk2q   // Output clock (quadrature)
+    output reg  clki,  // Output clock (in-phase)
+    output reg  clkq   // Output clock (quadrature)
 );
 
-    always @(rst) begin
-        clk2i <= 0;
-        clk2q <= 0;
-    end
-   
-    always @(posedge clk) begin
-        clk2i <= ~clk2i;
-    end
-
-    always @(negedge clk) begin
-        clk2q <= ~clk2q;
+    reg clki_started; // Ensure clki starts first.
+    
+    always @(clk or posedge rst) begin
+        if (rst) begin
+            clki <= 0;
+            clkq <= 0;
+            clki_started <= 0;
+        end 
+        else if (clk) begin
+            clki <= ~clki;
+	    clki_started <= 1;
+	end
+        else if (~clk && clki_started) clkq <= ~clkq;
     end
 
 endmodule
--- fpga/test/frequency_divider-tb.v
+++ fpga/test/frequency_divider-tb.v
@@ -1,22 +1,33 @@
 module tb;
     reg clk = 0;
-    reg rst = 1;
-    wire clk2i;
-    wire clk2q;
+    reg rst = 0;
+    wire clki;
+    wire clkq;
 
     frequency_divider fd(
         .clk(clk),
         .rst(rst),
-        .clk2i(clk2i),
-        .clk2q(clk2q)
+        .clki(clki),
+        .clkq(clkq)
     );
 
     always #1 clk = ~clk;
 
+//    sequence freq;
+//       (clki == 0) ##2 (clki == 0);
+//    endsequence
+//   
     initial begin
         $dumpfile("frequency_divider-tb.vcd");
         $dumpvars(1, tb);
-        #2 rst = 0;
-        #10 $finish;
+        #1 rst = 1;
+        #1 assert(clki == 0 && clkq == 0) 
+          else $error("Asynchronous reset failed.");
+        #2 rst = 0;       
+       // #1 (clki == 1 && clkq == 0)
+        #12 $finish;
     end
+
 endmodule
+
+
