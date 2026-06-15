20426b9988363736b82a34448362b31c86dfb749
Nogaye Bale <nogaye.bale@etu.umontpellier.fr>
2026-06-15 23:58:01 +0200

    Test Module top

--- test/Top_level-tb.v
+++ test/Top_level-tb.v
@@ -1,6 +1,11 @@
-`timescale 1ns/1ps
+module tb();
 
-module tb;
+  // Dump the signals to a FST file. You can view it with gtkwave or surfer.
+  initial begin
+    $dumpfile("Top_level-tb.fst");
+    $dumpvars(0, tb);
+    #1;
+  end
     parameter NF = 8;
     parameter NB = 8;
     parameter TOTAL_BITS = 2 * NF * NB;
@@ -13,20 +18,21 @@ module tb;
     //wire [TOTAL_BITS-1:0] Q_out;
     wire TX;
     wire [7:0] MUX_OUT;
-
+    wire dac_out;
     
     Top_level #(
         .NF(NF),
         .NB(NB),
         .TOTAL_BITS(TOTAL_BITS)
-    )dut (
+    )dut(
         .CLK(CLK),
         .RST(RST),
         .ADC_IN(ADC_IN),
         //.Q_out(Q_out)
         .TX (TX),
         .MUX_ADDR(MUX_ADDR),
-        .MUX_OUT(MUX_OUT)
+        .MUX_OUT(MUX_OUT),
+        .dac_out(dac_out)
     );
 
     wire ready;
